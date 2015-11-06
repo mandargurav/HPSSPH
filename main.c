af49474c15a6003ba1bb79fff883048d0ecb8692
Mandar P Gurav <134406005@iitb.ac.in>
2015-11-06 15:24:54 +0530

    added main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h> 
+#include<math.h>
+#include<string.h>
+#include<omp.h>
+
+
+int main(int argc, char **argv)
+{
+	printf("\n Hello from - High Performance Scalable Smoothed Particle Hydrodynamics!\n");
+	return 0;
+}
